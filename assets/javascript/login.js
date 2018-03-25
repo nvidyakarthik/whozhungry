8cb969f3a142eaa368f50396bf5bcab4375eab57
nvidya.karthik <nvidya.karthik@gmail.com>
2018-03-25 10:49:53 -0700

    Added login funtionality and logout using firebase

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,5 +1,15 @@
-
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD_p_80vQAC85hnttvurL1tDF9cR2cSCzA",
+  authDomain: "whozhungry-e0979.firebaseapp.com",
+  databaseURL: "https://whozhungry-e0979.firebaseio.com",
+  projectId: "whozhungry-e0979",
+  storageBucket: "whozhungry-e0979.appspot.com",
+  messagingSenderId: "235118731232"
+};
+firebase.initializeApp(config);
 var provider = new firebase.auth.GoogleAuthProvider();
+//This function is for the user who clicks the google signin button
 $("#google-signIn").on("click", function (event) {
   
     firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -7,7 +17,9 @@ $("#google-signIn").on("click", function (event) {
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
-        console.log("userName :"+user);
+        window.location.href="home.html";
+        console.log("google userName :"+user);
+       
         // ...
       }).catch(function(error) {
         // Handle Errors here.
@@ -20,26 +32,71 @@ $("#google-signIn").on("click", function (event) {
         // ...
       });
 });
-
-$("#google-signOut").on("click", function (event) {
-firebase.auth().signOut().then(function() {
-  // Sign-out successful.
+//This function is for the user who clicks the facebook signin button
+$("#facebook-signIn").on("click", function (event) {
+  provider=new firebase.auth.FaceBookAuthProvider();
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  window.location.href="home.html";
+  console.log("google userName :"+user);
+  // ...
 }).catch(function(error) {
-  // An error happened.
-});
-});
-
-$("#user-login").on("click", function (event) {
-//alert("test");
-var userName=$("username").val().trim();
-console.log(username);
-var password=$("password").val().trim();
-console.log(password);
-firebase.auth().signInWithEmailAndPassword(username, password).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
   // ...
+});
+});
+
+//This function registers new user
+$("#register-login").on("click", function (event) {
+  event.preventDefault();
+  var inputUsernameEmail=$("#inputUsernameEmail").val().trim();
+  console.log("username:"+inputUsernameEmail);
+  var inputPassword=$("#inputPassword").val().trim();
+  console.log(inputPassword);
+  //Create user with password
+   firebase
+   .auth()
+   .createUserWithEmailAndPassword(inputUsernameEmail, inputPassword).then(function(user){
+    window.location.href="home.html";   
+  
+  })
+   .catch(function(error) {
+     // Handle Errors here.
+     var errorCode = error.code;
+     var errorMessage = error.message;
+     
+   //Send user to homepage
+   });
+});
+
+
+$("#user-login").on("click", function (event) {
+//alert("test");
+event.preventDefault();
+var email=$("#inputUsernameEmail").val().trim();
+console.log("username:"+email);
+var password=$("#inputPassword").val().trim();
+
+firebase.auth().signInWithEmailAndPassword(email, password).
+then(function(user){
+  window.location.href="home.html";
+  
+
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  $("#errorMessage").empty();
+  $("#errorMessage").append("Incorrect Email or Password");
 });
 });
 
@@ -49,7 +106,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     //alert("signedin");
     var user = firebase.auth().currentUser;
     if(user!=null){
-      window.location("home.html");
+      
       console.log(user.email);
     }
   } else {
@@ -58,12 +115,15 @@ firebase.auth().onAuthStateChanged(function(user) {
     console.log("usersignedout");
   }
 });
-
-$("#logout").on("click", function (event) {
-  firebase.auth().signOut().then(function() {
+$(document).ready(function() {
+  $("#logout").on("click", function () {
+  
+   firebase.auth().signOut().then(function() {
     // Sign-out successful.
-    console.log("logout");
+    console.log("logout successful");
+    window.location.href="index.html";
   }).catch(function(error) {
     // An error happened.
-  });
+  }); 
+});
 });
