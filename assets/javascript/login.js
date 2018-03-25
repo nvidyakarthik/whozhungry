a4b051bc9e60a146ee08b1ecd8b0a8810cf471d5
nvidya.karthik <nvidya.karthik@gmail.com>
2018-03-24 22:45:06 -0700

    google signin and userlogin functionality added

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -28,3 +28,42 @@ firebase.auth().signOut().then(function() {
   // An error happened.
 });
 });
+
+$("#user-login").on("click", function (event) {
+//alert("test");
+var userName=$("username").val().trim();
+console.log(username);
+var password=$("password").val().trim();
+console.log(password);
+firebase.auth().signInWithEmailAndPassword(username, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+});
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    //alert("signedin");
+    var user = firebase.auth().currentUser;
+    if(user!=null){
+      window.location("home.html");
+      console.log(user.email);
+    }
+  } else {
+    // No user is signed in.
+    //alert("signedout");
+    console.log("usersignedout");
+  }
+});
+
+$("#logout").on("click", function (event) {
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    console.log("logout");
+  }).catch(function(error) {
+    // An error happened.
+  });
+});
