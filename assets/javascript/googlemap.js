27685c54bd18113e44c168af021ea752b409057e
nvidya.karthik <nvidya.karthik@gmail.com>
2018-03-26 12:47:13 -0700

    google maps search functionality added

--- assets/javascript/googlemap.js
+++ assets/javascript/googlemap.js
@@ -5,9 +5,77 @@
 var map;
 var infowindow;
 
-function initMap() {
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+   
+    center: {lat: 33.360355, lng: -111.801682},
+    zoom: 13,
+    mapTypeId: 'roadmap'
+  });
 
-    var pyrmont = {lat: -33.867, lng: 151.195};
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+
+ function initMap() {
+
+    var pyrmont = {lat: 33.360355, lng: -111.801682};
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: pyrmont,
@@ -19,7 +87,7 @@ function initMap() {
   service.nearbySearch({
     location: pyrmont,
     radius: 500,
-    type: ['store']
+    type: ['food']
   }, callback);
 }
 
@@ -42,4 +110,4 @@ function createMarker(place) {
     infowindow.setContent(place.name);
     infowindow.open(map, this);
   });
-}
+}  
